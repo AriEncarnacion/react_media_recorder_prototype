3bf552e7711104c1cab7439eff66fd15907e90ec
AriEncarnacion <ari.encarnacion.95@gmail.com>
2021-10-24 03:07:37 -0700

    init commit

--- src/components/AudioRecorder.js
+++ src/components/AudioRecorder.js
@@ -1,9 +1,35 @@
 import React, {useEffect, useState} from "react";
+import {ReactMic} from 'react-mic';
 
 const AudioRecorder = () => {
+  const [record, setRecord] = useState(false);
+
+  const startRecording = () => {
+    setRecord(true);
+  };
+
+  const stopRecording = () => {
+    setRecord(false);
+  };
+
+  const onData = (recordedBlob) => {
+    // console.log('chunk of real-time data is: ', recordedBlob);
+  }
+
+  const onStop = (recordedBlob) => {
+    console.log('recordedBlob is: ', recordedBlob);
+  }
 
   return (
-    <div>audio recorder
+    <div>
+      <header>audio recorder</header>
+      <ReactMic
+        record={record}
+        className={"reactMic-recorder"}
+        onStop={onStop}
+        onData={onData} />
+      <button onClick={startRecording} type="button">Start</button>
+      <button onClick={stopRecording} type="button">Stop</button>
     </div>
   )
 }
